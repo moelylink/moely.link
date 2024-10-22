2aa0b9a6f0836ea8282acbd7fc98afc39dce391c
moelylink <110507373+abcxyz-123456@users.noreply.github.com>
2024-10-22 16:29:51 +0000

    更新

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/7.1.0/workbox-sw.js');
 
 // 缓存版本号
-let cacheVersion = '-241021';
+let cacheVersion = '-241022';
 // 最大条目数
 const maxEntries = 500;
 
